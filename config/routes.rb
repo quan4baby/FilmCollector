f9eb652fa7439e165deef0320606f3e9d2542796
Shaquan Patterson <pattersons3@chc.edu>
2020-09-17 10:46:41 -0400

    Completed Create Actor and View Movies, and started Create Movie

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   get "actors" => "actors#index"
   get "actors/new" => "actors#new"
   post "actors" => "actors#create"
+  get "movies" => "movies#index"
+  get "movies/new" => "movies#new"
+  post "movies" => "movies#create"
 end
